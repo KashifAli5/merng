19f7c69b921605fa68134973db391b431c67693a
Kashif Ali <kashifali.dev@zohomail.com>
2021-03-14 04:03:08 +0500

    spliting code into seprate files

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag'); 
+
 const mongoose = require('mongoose');   
 const { MONGODB } = require('./config');
 
 
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs'); 
+const resolvers = require('./graphql/resolvers')
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-  }
-};
 
 const server = new ApolloServer({
     typeDefs,
@@ -41,7 +18,7 @@ mongoose
     .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
         console.log('DB connnected');
-        return server.listen({ port: 5005});
+        return server.listen({ port: 5006});
     })
     .then((res) => {
         console.log(`server running at ${res.url}`)
